baf7d9d801a322a5880fa787cd731000e93072bf
komal030 <96829750+komal030@users.noreply.github.com>
2023-07-03 16:24:46 +0530

    Update 125.ValidPalindrome.java

--- 125.ValidPalindrome.java
+++ 125.ValidPalindrome.java
@@ -1,1 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String p = "";
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9')
+                p = p + ch;
+        }
+        for (int i = 0; i < p.length() / 2; i++) {
+            if (p.charAt(i) != p.charAt(l - 1 - i))
+                return false;
+        }
+        return true;
+    }
+}
 
