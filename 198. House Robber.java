87e365890d584ed0d18aaf40e2cbd9241ca8128c
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-05 22:26:38 +0530

    Create 198. House Robber.java

---
+++ 198. House Robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1)
+        return nums[0];
+        if(n==2)
+        return Math.max(nums[0],nums[1]);
+        nums[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+            {
+                nums[i]=Math.max(nums[i]+nums[i-2],nums[i-1]);
+            }
+        return nums[nums.length-1];
+    }
+}
