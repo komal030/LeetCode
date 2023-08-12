684b71683bf04d27430ec55cc81afa657105fd9d
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-12 13:37:06 +0530

    Update 5. Longest Palindromic Substring.java

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -1,44 +1,33 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        String p="";
-        for(int i=0;i<n;i++)
-        {
-            int l=i,r=i;
-            String temp="";
-            while(l>=0 && r<n)
-            {
-                if(s.charAt(l)==s.charAt(r))
-                {
-                    temp=s.substring(l,r+1);
-                    l--;
-                    r++;
-                }
-                else break;
-            }
-            if(temp.length()>p.length())
-            p=temp;
+  public String longestPalindrome(String s) {
+    int n = s.length(),p = 0;
+    String temp = "";
+    for (int i = 0; i < n; i++) {
+      int l = i;
+      int r = i;
+      while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+        if (r - l + 1 > p) {
+          temp = s.substring(l, r + 1);
+          p = r - l + 1;
         }
+        l--;
+        r++;
 
-for(int i=0;i<n;i++)
-        {
-            int l=i;
-            int r=i+1;
-            String temp="";
-            while(l>=0 && r<n)
-            {
-                if(s.charAt(l)==s.charAt(r))
-                {
-                    temp=s.substring(l,r+1);
-                    l--;
-                    r++;
-                }
-                else break;
-            }
-            if(temp.length()>p.length())
-            p=temp;
-        }
-
-        return p;
+      }
     }
+    for (int i = 0; i < n; i++) {
+      int l = i;
+      int r = i + 1;
+      while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+        if (r - l + 1 > p) {
+          temp = s.substring(l, r + 1);
+          p = r - l + 1;
+        }
+        l--;
+        r++;
+
+      }
+    }
+    return temp;
+  }
 }
