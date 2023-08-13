3f1f5220f666a548ef6e9ace89c726ebb1990fc5
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-13 08:37:26 +0530

    Update 74. Search a 2D Matrix.java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -2,20 +2,33 @@ class Solution {
   public boolean searchMatrix(int[][] matrix, int x) {
     int m = matrix.length;
     int n = matrix[0].length;
-        int l= 0, r = m*n - 1;
-    
-        while (l <= r) {
-          int mid=(l+(r-l)/2);
-          int midr = mid / n;
-          int midc=mid-midr*n;
+    int l = 0, r = m - 1, mid = 0;
 
-          if (matrix[midr][midc] == x)
-            return true;
-          else if (matrix[midr][midc] > x)
-            r = mid - 1;
-          else
-            l = mid + 1;
-        }   
+    while (l <= r) {
+      mid = (l + r) / 2;
+      if (matrix[mid][0] > x)
+        r = mid - 1;
+      else if (matrix[mid][n - 1] < x)
+        l = mid + 1;
+      //else if(matrix[mid][0] == x || matrix[mid][n-1] == x)
+      //break;
+      else break;
+    }
+    if (!(l <= r))
+      return false;
+    l = 0;
+    r = n - 1;
+
+    while (l <= r) {
+      int mid1 = (l + r) / 2;
+      if (matrix[mid][mid1] == x)
+        return true;
+      else if (matrix[mid][mid1] > x)
+        r = mid1 - 1;
+      else
+        l = mid1 + 1;
+    }
+
     return false;
   }
 }
