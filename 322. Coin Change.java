91d55b508d68d99f809b78ea85153d4763a25c71
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-13 09:40:21 +0530

    Update 322. Coin Change.java

--- 322. Coin Change.java
+++ 322. Coin Change.java
@@ -1,17 +1,16 @@
 class Solution {
   public int coinChange(int[] coins, int amount) {
-    Arrays.sort(coins);
     int n = coins.length;
     int dp[] = new int[amount + 1];
-    Arrays.fill(dp, Integer.MAX_VALUE);
+    Arrays.fill(dp, amount+1);
     dp[0] = 0;
     for (int i = 1; i <= amount; i++) {
       for (int j = 0; j < n; j++) {
-        if (coins[j] <= i && dp[i - coins[j]] != Integer.MAX_VALUE)
+        if (coins[j] <= i && dp[i - coins[j]] != amount+1)
           dp[i] = Math.min(1 + dp[i - coins[j]], dp[i]);
       }
     }
-    if (dp[amount] == Integer.MAX_VALUE)
+    if (dp[amount] == amount+1)
       return -1;
     else return dp[amount];
   }
