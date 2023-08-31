790bf691d3f6d6dd001ebe6ddc699cf9ffc9da90
komal030 <96829750+komal030@users.noreply.github.com>
2023-08-31 18:11:03 +0530

    Update 213. House Robber II.java

--- 213. House Robber II.java
+++ 213. House Robber II.java
@@ -1,23 +1,29 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        int arr[]=nums.clone();
-        if(n==1)
-        return nums[0];
-        if(n==2)
-        return Math.max(nums[0],nums[1]);
-      nums[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<n-1;i++)
-            {
-                nums[i]=Math.max(nums[i]+nums[i-2],nums[i-1]);
-            }
-
-         arr[2]=Math.max(arr[1],arr[2]);
-   for(int i=3;i<n;i++)
-            {
-                arr[i]=Math.max(arr[i]+arr[i-2],arr[i-1]);
-            }
-        return Math.max(nums[n-2],arr[n-1]);
-
+       int n=nums.length;
+       if(n==1)
+       return nums[0];
+       int res1,res2,temp;
+       int max1=nums[0];
+       int max2=0;
+       for(int i=1;i<n-1;i++)
+       {
+        temp=max1;
+        max1=Math.max(max1,max2+nums[i]);
+        max2=temp;   
+       }
+       res1=Math.max(max1,max2);
+       max1= nums[1];
+       max2=0;
+        for(int i=2;i<n;i++)
+        {
+         temp=max1;
+         max1=Math.max(max1,nums[i]+max2);
+         max2=temp;   
+        }
+        res2=Math.max(max1,max2);
+        return Math.max(res1,res2);
     }
 }
+
+        
