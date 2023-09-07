f22c927a68a90d8d06bb166f151d78a14f42942d
komal030 <96829750+komal030@users.noreply.github.com>
2023-09-07 13:06:46 +0530

    Create 125. Valid Palindrome.java

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String p = "";
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9')
+                p = p + ch;
+        }
+        for (int i = 0; i < p.length() / 2; i++) {
+            if (p.charAt(i) != p.charAt(p.length() - 1 - i))
+                return false;
+        }
+        return true;
+    }
+}
